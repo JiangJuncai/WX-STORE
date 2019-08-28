0a6506ebb5f5f5934300347fa57f5214390a8d55
stans <834410151@qq.com>
2019-08-28 22:27:55 +0800

    recommendfood location phonecall

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,5 +5,36 @@ Page({
   data: {
     imgUrls: ['../../images/swiper1.jpg', '../../images/swiper2.jpg', '../../images/swiper3.jpg']
   },
-  onLoad: function() {}
+  onLoad: function() {},
+  goLocation: function() {
+    wx.navigateTo({
+      url: '../location/location'
+    })
+  },
+  makePhoneCall: function() {
+    wx.makePhoneCall({
+      phoneNumber: '18655329580'
+    })
+  },
+  goOrderPage: function() {
+    wx.switchTab({
+      url: '../order/order'
+    })
+  },
+  payMoney: function() {
+    wx.requestPayment({
+      timeStamp: '',
+      nonceStr: '',
+      package: '',
+      signType: 'MD5',
+      paySign: '',
+      success: function(res) {
+        console.log(123)
+      },
+      fail: function(res) {
+        console.log(444)
+      },
+      complete: function(res) {}
+    })
+  }
 })
---
+++ miniprogram/pages/location/location.js
@@ -0,0 +1,18 @@
+// miniprogram/pages/location/location.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    markers: [
+      {
+        iconPath: '../../images/location.svg',
+        id: 0,
+        latitude: 34.5829139,
+        longitude: 115.40457446,
+        width: 16,
+        height: 16
+      }
+    ]
+  }
+})
