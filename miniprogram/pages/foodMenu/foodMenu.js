83a94d8cdf13f14747bc1bd65d68674b685b745f
stans <834410151@qq.com>
2019-09-01 22:31:32 +0800

    complete foodmenu, coupon

--- miniprogram/pages/foodMenu/foodMenu.js
+++ miniprogram/pages/foodMenu/foodMenu.js
@@ -10,7 +10,10 @@ Page({
     currentIndex: 0,
     currentMenu: '',
     foods: [],
-    cateTotal: {} // 每个分类中选中商品的数量
+    cateTotal: {}, // 每个分类中选中商品的数量
+    cartList: [],
+    totalCount: 0, // 购买商品的总数
+    totalPrice: 0
   },
   selectCate: function(e) {
     const index = e.currentTarget.dataset.index
@@ -23,23 +26,70 @@ Page({
     const currentMenu = this.data.currentMenu
     const index = e.currentTarget.dataset.index
     let foodlist = this.data.foods
+    const currentFood = foodlist[currentMenu][index]
     if (e.currentTarget.dataset.func === 'add') {
-      foodlist[currentMenu][index].count += 1
+      currentFood.count += 1
     } else {
-      foodlist[currentMenu][index].count -= 1
+      currentFood.count -= 1
     }
-    const count = this.refreshCount(foodlist[[currentMenu]])
+    this.addToCart(currentFood)
+    const count = this.refreshCateCount(foodlist[[currentMenu]])
     const ct = this.data.cateTotal
     ct[currentMenu] = count
+
+    const tc = this.refreshTotalData()
     this.setData({
       foods: foodlist,
       cateTotal: ct
     })
   },
-  refreshCount: function(arr) {
-    return arr.reduce((total, i) => {
-      return total + i.count
+  /**
+   * 刷新每个分类里已购商品的数量
+   * @param {Array} arr
+   */
+  refreshCateCount: function(arr) {
+    return arr.reduce((total, item) => {
+      return total + item.count
     }, 0)
+  },
+  /**
+   * 刷新所有已购商品的数量
+   * @param {Array} arr
+   */
+  refreshTotalData: function() {
+    const cartList = this.data.cartList
+    const totalCount = cartList.reduce((total, item) => {
+      return total + item.count
+    }, 0)
+    const totalPrice = cartList.reduce((total, item) => {
+      return total + item.price * item.count
+    }, 0)
+    this.setData({
+      totalCount: totalCount,
+      totalPrice: totalPrice
+    })
+  },
+  /**
+   * 将商品添加到购物车
+   * @param {Object} obj
+   */
+  addToCart: function(obj) {
+    const cartList = this.data.cartList
+    const index = cartList.findIndex(item => {
+      return item.name === obj.name
+    })
+    if (index >= 0) {
+      if (cartList[index].count === 0) {
+        cartList.splice(index, 1)
+      } else {
+        cartList[index] = obj
+      }
+    } else {
+      cartList.push(obj)
+    }
+    this.setData({
+      cartList: cartList
+    })
   },
 
   /**
